191e270516f737830654fc40a4ca7cbc1d81fd5f
SchemaMarkApp <backandfrontmarketing@gmail.com>
2024-07-29 09:35:07 -0400

    test

--- content.js
+++ content.js
@@ -1,14 +1,16 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'extractSchema') {
-    const schemas = [];
-    document.querySelectorAll('script[type="application/ld+json"]').forEach((script) => {
-      try {
-        const json = JSON.parse(script.innerText);
-        schemas.push(json);
-      } catch (e) {
-        console.error('Error parsing JSON:', e);
-      }
-    });
-    sendResponse({ schema: schemas });
-  }
-});
+console.log('Content script loaded.');
+   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+       console.log('Message received:', request);
+       if (request.action === 'extractSchema') {
+           const schemas = [];
+           document.querySelectorAll('script[type="application/ld+json"]').forEach((script) => {
+               try {
+                   const json = JSON.parse(script.innerText);
+                   schemas.push(json);
+               } catch (e) {
+                   console.error('Error parsing JSON:', e);
+               }
+           });
+           sendResponse({ schema: schemas });
+       }
+   });
